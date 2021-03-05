481ea94dd5491bc3c705eaf50d175344b10a9185
Tejas Tawde <tejascodes.281@gmail.com>
2021-03-05 23:24:20 +0530

    Day-2 Commit

---
+++ flutter_catalog/lib/homepage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    String str = "#30DaysOfFlutter";
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $str!"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
