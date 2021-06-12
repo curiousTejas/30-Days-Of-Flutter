681d5e52a25de626eb81a7f3d4487736d6c09257
Tejas Tawde <tejascodes.281@gmail.com>
2021-06-12 17:46:50 +0530

    Day-10 Completed

---
+++ flutter_catalog/lib/models/catalog.dart
@@ -0,0 +1,13 @@
+class Item {
+  final String id;
+  final String name;
+  final String descriptn;
+  final num price;
+  final String color;
+  final String image;
+
+  Item(
+      {this.id, this.name, this.descriptn, this.price, this.color, this.image});
+}
+
+final products = [Item()];
