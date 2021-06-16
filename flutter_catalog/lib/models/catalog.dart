9307d6e4ba361efabfb212d7ab1a2d3edee6d97b
Tejas Tawde <tejascodes.281@gmail.com>
2021-06-16 16:47:32 +0530

    Day-10 minor additions

--- flutter_catalog/lib/models/catalog.dart
+++ flutter_catalog/lib/models/catalog.dart
@@ -10,4 +10,14 @@ class Item {
       {this.id, this.name, this.descriptn, this.price, this.color, this.image});
 }
 
-final products = [Item()];
+final products = [
+  Item(
+    id: "TeeCatalog001",
+    name: "Google Pixel 4a",
+    descriptn: "Google Pixel phone from the 4th Generation",
+    price: 29999,
+    color: "#000000",
+    image:
+        "https://images-na.ssl-images-amazon.com/images/I/7199N-Uz2AL._SX679_.jpg",
+  )
+];
