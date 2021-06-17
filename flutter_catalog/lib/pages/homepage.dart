0b7196f95131d07835496b7dc31c107e0d090892
Tejas Tawde <tejascodes.281@gmail.com>
2021-06-17 13:40:54 +0530

    Day-13 Completed

--- flutter_catalog/lib/pages/homepage.dart
+++ flutter_catalog/lib/pages/homepage.dart
@@ -1,9 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
 import 'package:flutter_catalog/widgets/itemWidget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    // ignore: todo
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJSON =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decoded = jsonDecode(catalogJSON);
+    final productData = decoded["products"];
+    print(productData);
+  }
+
   @override
   Widget build(BuildContext context) {
     // String str = "#30DaysOfFlutter";
