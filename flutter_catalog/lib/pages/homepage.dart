7fd631bb1762b196d8ab753dff5af9b03e063fdc
Tejas Tawde <tejascodes.281@gmail.com>
2021-06-17 16:08:49 +0530

    Day-16 Completed

--- flutter_catalog/lib/pages/homepage.dart
+++ flutter_catalog/lib/pages/homepage.dart
@@ -39,11 +39,47 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-            ? ListView.builder(
+            ? GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 16,
+                  crossAxisSpacing: 16,
+                ),
+                itemBuilder: (context, index) {
+                  final item = CatalogModel.items[index];
+                  return Card(
+                      clipBehavior: Clip.antiAlias,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: GridTile(
+                          child: Image.network(item.image),
+                          header: Container(
+                            child: Text(
+                              item.name,
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                            padding: const EdgeInsets.all(12),
+                            decoration: BoxDecoration(
+                              color: Colors.deepPurple,
+                            ),
+                          ),
+                          footer: Container(
+                            child: Text(
+                              "₹" + item.price.toString(),
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                            padding: const EdgeInsets.all(12),
+                            decoration: BoxDecoration(
+                              color: Colors.black,
+                            ),
+                          )));
+                },
                 itemCount: CatalogModel.items.length,
-                itemBuilder: (context, index) => ItemWidget(
-                      item: CatalogModel.items[index],
-                    ))
+              )
             : Center(
                 child: CircularProgressIndicator(),
               ),
