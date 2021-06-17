14e2f2c350cfa54960a9d30b008ff27edaff95c5
Tejas Tawde <tejascodes.281@gmail.com>
2021-06-17 19:05:33 +0530

    Day-17 completed

--- flutter_catalog/lib/widgets/themes.dart
+++ flutter_catalog/lib/widgets/themes.dart
@@ -4,7 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 class MyTheme {
   static ThemeData lightTheme(BuildContext context) => ThemeData(
       primarySwatch: Colors.deepPurple,
-      fontFamily: GoogleFonts.lato().fontFamily,
+      fontFamily: GoogleFonts.mukta().fontFamily,
       appBarTheme: AppBarTheme(
         color: Colors.white,
         elevation: 0.0,
@@ -14,4 +14,7 @@ class MyTheme {
   static ThemeData darkTheme(BuildContext context) => ThemeData(
         brightness: Brightness.dark,
       );
+
+  static Color cream = Color(0xfff5f5f5);
+  static Color darkerBlue = Color(0xff403b58);
 }
