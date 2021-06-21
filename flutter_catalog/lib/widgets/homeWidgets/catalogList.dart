ee99c0413d9f11e378fdb092fb034c2ae58fa4f2
Tejas Tawde <tejascodes.281@gmail.com>
2021-06-21 16:17:24 +0530

    Day-25 Complete

--- flutter_catalog/lib/widgets/homeWidgets/catalogList.dart
+++ flutter_catalog/lib/widgets/homeWidgets/catalogList.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/widgets/homeWidgets/addToCart.dart';
 import 'package:velocity_x/velocity_x.dart';
-
-import 'package:flutter_catalog/models/cart.dart';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/pages/homeDetail.dart';
 import 'package:flutter_catalog/widgets/homeWidgets/catalogImage.dart';
@@ -56,7 +55,7 @@ class CatalogItem extends StatelessWidget {
                 buttonPadding: EdgeInsets.zero,
                 children: [
                   "₹ ${catalog.price}".text.bold.xl.make(),
-                  _AddToCart(
+                  AddToCart(
                     catalog: catalog,
                   )
                 ],
@@ -66,37 +65,5 @@ class CatalogItem extends StatelessWidget {
         )
       ],
     )).color(context.cardColor).roundedLg.square(150).make().py12();
-  }
-}
-
-class _AddToCart extends StatefulWidget {
-  final Item catalog;
-  const _AddToCart({
-    Key key,
-    this.catalog,
-  }) : super(key: key);
-
-  @override
-  __AddToCartState createState() => __AddToCartState();
-}
-
-class __AddToCartState extends State<_AddToCart> {
-  bool isAdded = false;
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-        onPressed: () {
-          isAdded = isAdded.toggle();
-          final _catalog = CatalogModel();
-          final _cart = CartModel();
-          _cart.catalog = _catalog;
-          _cart.add(widget.catalog);
-          setState(() {});
-        },
-        style: ButtonStyle(
-            backgroundColor:
-                MaterialStateProperty.all(context.theme.buttonColor),
-            shape: MaterialStateProperty.all(StadiumBorder())),
-        child: isAdded ? Icon(Icons.done) : "Add to Cart".text.make());
   }
 }
